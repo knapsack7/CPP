ed87a0c76c54b125114f37c32f25b51fc831a063
Knapsack120 <knapsack120@gmail.com>
2019-06-08 12:31:20 +0530

    Update KMP.cpp

--- String Algorithms/KMP.cpp
+++ String Algorithms/KMP.cpp
@@ -1,35 +1,51 @@
-//every point in longestPrefix vector will tell us what is the longest suffix length which is also
-//the same as prefix
-vector<int> computePrefix(string pat){
-    int m = pat.size();
-    vector<int> longestPrefix(m);
-    for(int i = 1, j = 0; i < m; i++){
-        while(j > 0 && pat[j] != pat[i]){
-            j = longestPrefix[j - 1];
+//TC-O(N+m)
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> computePrefixLength(string pat){//TC-O(N)
+    int len=pat.length();
+    vector<int> longestPrefix(len);
+    for(int i=1,j=0;i<len;++i){
+        while(j>0 && pat[i]!=pat[j]){
+            j=longestPrefix[j-1];
         }
-        if(pat[i] == pat[j]){
-            longestPrefix[i] = ++j;
-        }
-        else{
-            longestPrefix[i] = j;
+        if(pat[i]==pat[j]){
+            longestPrefix[i]=++j;
+        }else{
+            longestPrefix[i]=longestPrefix[j];
         }
     }
     return longestPrefix;
 }
+
 void KMP(string str,string pat){
     int n = str.size();
     int m = pat.size();
-    vector<int> longestPrefix = computePrefix(pat);
-    for(int i = 0, k = 0; i < n; i++){
-        while(k > 0 && pat[k] != str[i]){
-            k = longestPrefix[k - 1];
+    vector<int> longestPrefix = computePrefixLength(pat);
+    int cnt=0;
+    for(int i=0,k=0;i<n;i++){
+        while(k>0&&pat[k]!=str[i]){
+            k=longestPrefix[k-1];
         }
-        if(str[i] == pat[k]){
+        if(str[i]==pat[k]){
             k++;
         }
-        if(k == m){
-            cout << i - m + 1 << "\n";
-            k = longestPrefix[k - 1];
+        if(k==m){
+            //cout << i - m + 1 << "\n";//for indexes
+            cnt++;//count no of occurences
+            k=longestPrefix[k-1];
         }
     }
+    cout<<cnt;
+}
+
+
+int main(){
+    string txt="";cin>>txt;
+    string pattern="";cin>>pattern;
+    // for(int i=0;i<(int)prefix.size();++i)
+    //     cout<<prefix[i]<<" ";
+    // cout<<endl;
+    KMP(txt,pattern);
+    return 0;
 }
