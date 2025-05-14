8498e1370796ecbbebf4bd6aea270dad4bc14e98
knapsack7 <knapsack120@gmai.com>
2025-05-14 13:25:50 +0530

    multithreading_updation

--- multithreading/thread/generic_function/main.cpp
+++ multithreading/thread/generic_function/main.cpp
@@ -7,7 +7,19 @@ void printMessage(){
 
 int main(){
 	std::thread t(printMessage); // create a thread and runs printMessage
+	 // join() waits for the thread to complete
+    // If you don't join, program might terminate before thread completes
+
 	if(t.joinable())
 		t.join();
+	
+	std::thread t2([](){
+		std::cout<<"Thread2 Running\n"<<std::endl;
+	});
+
+	if (t2.joinable()){
+		t2.join();
+	}
+
 	return 0;
 }
