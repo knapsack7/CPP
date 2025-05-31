f6b865660ea3d68e3ac23d1b9b4f17f00d972450
knapsack7 <knapsack120@gmai.com>
2025-05-31 17:33:49 +0530

    singleton_updation

--- designPatterns/creational/singleton/main.cpp
+++ designPatterns/creational/singleton/main.cpp
@@ -3,6 +3,15 @@
 #include<mutex>
 #include<fstream> /*to read and write to the files*/
 
+/*
+Always remember:
+An object is constructed in this order:
+1) its virtual base gets constructed,
+2) then its base,
+3) then its members,
+4) then the constructor body runs.
+*/
+
 class Logger{
 private:
 	std::ofstream logFile; /*to write data in files*/
@@ -59,3 +68,15 @@ int main() {
     std::cout << "Check the log.txt file for the logged messages." << std::endl;
     return 0;
 }
+
+/*
+Key points:
+1. Made the constructor private to prevent instantiation
+2. Made an function static to get the instance of the class
+3. Made an static data member to store the instance of the class
+4. Note only static data members are initialized when the class is loaded, not when an object is created
+5. Static member function can use only static data members
+6. static data members are initialized before the constructor is called
+7. Statid data members need to be initialized outside the class (if not using inline keyword).
+8. Need to add check in the getInstance function to avoid multiple instances of the class
+*/
