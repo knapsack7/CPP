6bfa942e9977ce019ad8d576e19c997a1bd1a9a1
knapsack7 <knapsack120@gmai.com>
2025-06-09 19:03:02 +0530

    dynamic_cast

--- oops/src/main.cpp
+++ oops/src/main.cpp
@@ -4,39 +4,20 @@
 #include "../include/checking.h"
 #include "../include/transaction.h"
 #include <typeinfo>
-#include <cxxabi.h>
 
-
-// Function to demangle type names
-std::string demangle(const char* mangled_name) {
-    int status;
-    char* demangled = abi::__cxa_demangle(mangled_name, nullptr, nullptr, &status);
-    if (status == 0) {
-        std::string result(demangled);
-        free(demangled);
-        return result;
-    }
-    return mangled_name;
-}
 
 int main(){
-    Checking acc("Bob", 100, 50);
-    Account *p = &acc;
-    // transact(&acc);
+    Savings acc("Bob", 100, 50);
+    //transact(acc);
     // std::cout <<"Size of Account: " << sizeof(Account) << std::endl; // 4 bytes for vtable pointer
     
-    int i{};
-
-    float f{};
-    const std::type_info &ti = typeid(acc);
-
-    if (typeid(acc) == typeid(Checking)){
-        std::cout<< "acc is a Checking object" << std::endl;
+    try{
+        transact(acc);
+    }catch(const std::exception &e){
+        std::cout<< "Error: " << e.what() << std::endl;
     }
-    else{
-        std::cout<< "acc is not a Checking object" << std::endl;
-    }
-    
+
+
     return 0;
 }
 
--- oops/src/transaction.cpp
+++ oops/src/transaction.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include "../include/account.h"
 #include "../include/checking.h"
+
+
 void transact(Account *pAccount){
     std::cout<< "Transaction started" << std::endl;
     std::cout<< "Initial balance: " << pAccount->getBalance() << std::endl;
     pAccount->deposit(100);
     pAccount->accumulateInterest();
-    Checking *pCh = static_cast<Checking*>(pAccount);
-    std::cout<< "Min balance: " << pCh->getMinBalance() << std::endl;
+    Checking *pCheck = dynamic_cast<Checking*>(pAccount);
+    // dynamic cast returns nullptr for pointers if the cast is not possible
+    // so we can check if the cast is successful
+    if (pCheck){
+        std::cout<< "Min balance of Checking : " << pCheck->getMinBalance() << std::endl;
+    }
     pAccount->withdraw(170);
     std::cout<< "Interest: " << pAccount->getInterestRate() << std::endl;
     std::cout<< "Final balance: " << pAccount->getBalance() << std::endl;
+    std::cout<< "Transaction ended" << std::endl;
+}
+
+// dynammic cast also works with references
+
+
+void transact(Account &pAccount){
+    std::cout<< "Transaction started" << std::endl;
+    std::cout<< "Initial balance: " << pAccount.getBalance() << std::endl;
+    pAccount.deposit(100);
+    pAccount.accumulateInterest();
+    Checking &pCheck = dynamic_cast<Checking&>(pAccount);
+    // dynamic cast returns exception (bad_cast) for reference if the cast is not possible
+    // so we can check if the cast is successful
+    std::cout<< "Min balance of Checking : " << pCheck.getMinBalance() << std::endl;
+    pAccount.withdraw(170);
+    std::cout<< "Interest: " << pAccount.getInterestRate() << std::endl;
+    std::cout<< "Final balance: " << pAccount.getBalance() << std::endl;
     std::cout<< "Transaction ended" << std::endl;
 }
